97d07056555c6d57c0a39a7b8d6fc5cf18ff25aa
milan-scis <mils2503@gmail.com>
2024-03-17 19:37:57 +0100

    Fix - sections links

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -51,16 +51,3 @@ setTimeout(function () {
     }, 800);
 }, 1650);
 
-$(document).ready(function () {
-    $("a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('body,html').animate({
-                scrollTop: $(hash).offset().top
-            }, 1800, function () {
-                window.location.hash = hash;
-            });
-        }
-    });
-});
